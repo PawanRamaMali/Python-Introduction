c58d2653d8cafb01a8a1a80c88f78a15fda490b5
Pawan Rama Mali <prm@outlook.in>
2021-11-28 16:49:18 +0530

    Create main0.py

---
+++ Day 1/main0.py
@@ -0,0 +1,14 @@
+# Day 1 - New Name Generator
+# Working with Variables in Python to Manage Data
+
+print("Welcome to the Name Generator.")
+pet = input("What's your pet's name?\n").strip()
+street = input("What's name of the street you grew up in?\t").strip()
+city = input("Enter the city you like : ").strip()
+#print("Your band name could be " + street + " " + pet)
+print('Your new name could be {1} {2} {0} {1}'.format(street ,pet ,city))
+#print(f'Your band name could be {street} {city} {city} {pet}')
+
+#Syntax Error
+#Type Error
+#Value Error
