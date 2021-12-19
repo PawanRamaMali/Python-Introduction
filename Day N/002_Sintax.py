a79634c51385feaa8770d9d185e804eea1be5ecb
Pawan Rama Mali <prm@outlook.in>
2021-12-19 16:06:58 +0530

    Updated code

---
+++ Day N/002_Sintax.py
@@ -0,0 +1,9 @@
+# You must at least use a space or it won't compile
+
+x = int(input("Enter value of x : "))
+y= int(input("Enter value of y : "))
+
+if x > y:
+  print("x is greater than y!") 
+else:
+  print("x is smaller than y!")
